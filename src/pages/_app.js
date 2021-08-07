dfe15c9fc0693b761e5fc859b270ebfd38c95f6d
Will Johnston <wbjohnston@gmail.com>
2021-08-06 18:26:41 -0700

    initialize frontend

---
+++ src/pages/_app.js
@@ -0,0 +1,8 @@
+
+
+import '../styles.css';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+    return <Component { ...pageProps } />
+}
