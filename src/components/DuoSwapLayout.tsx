f903dfdd4b7b61a769d272f3bd8b2c24b751e6c5
Will Johnston <wbjohnston@gmail.com>
2021-08-08 11:17:43 -0700

    extract connection button into own component

--- src/components/DuoSwapLayout.tsx
+++ src/components/DuoSwapLayout.tsx
@@ -7,31 +7,36 @@ import Logo from "../public/logo.svg"
 import { useEthers, useEtherBalance } from "@usedapp/core";
 import { BigNumber, ethers } from 'ethers'
 
-export default function DuoSwapLayout({ children }: { children: Component | Component[] }) {
+interface WalletConnectionButtonProps {
+    activateWallet: any;
+    deactiveWallet: any;
+    account: string | null | undefined;
+}
 
-    const { activateBrowserWallet, deactivate, account } = useEthers();
-    const etherBalance: BigNumber | undefined = useEtherBalance(account);
+function WalletConnectionButton({ activateWallet, deactiveWallet, account }: WalletConnectionButtonProps) {
+    const etherBalance = useEtherBalance(account);
 
     function onActivateButtonClick() {
-        activateBrowserWallet();
+        activateWallet();
     }
 
     function onDisconnectButtonClick() {
-        deactivate();
+        deactiveWallet();
     }
 
-    console.log(account, etherBalance);
+    return account && etherBalance !== undefined
+        ? (<Button type="primary" style={{ float: "right" }} onClick={onDisconnectButtonClick}>{ethers.utils.formatEther(etherBalance)} ETH</Button>)
+        : (<Button type="primary" style={{ float: "right" }} onClick={onActivateButtonClick}>Connect Wallet</Button>)
+}
 
+export default function DuoSwapLayout({ children }: { children: Component | Component[] }) {
+    const { activateBrowserWallet, deactivate, account } = useEthers();
 
     return <Layout className="layout">
         <Header>
             <Image src={Logo} width={100} height={50} alt="logo" />
             <span style={{ color: 'white', justifyContent: 'center' }}>Hotdog Swap</span>
-            {
-                account !== undefined && etherBalance !== undefined
-                    ? (<Button type="primary" style={{ float: "right" }} onClick={onDisconnectButtonClick}>{ethers.utils.formatEther(etherBalance)} ETH</Button>)
-                    : (<Button type="primary" style={{ float: "right" }} onClick={onActivateButtonClick}>Connect Wallet</Button>)
-            }
+            <WalletConnectionButton activateWallet={activateBrowserWallet} deactiveWallet={deactivate} account={account} />
         </Header>
         <Content style={{ padding: '0 50px' }}>
             <div className="site-layout-content">
